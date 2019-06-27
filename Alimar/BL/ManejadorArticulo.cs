7b8d9ef283747177786375d99e7a90c5ce129eb6
tannia elizondo cespedes <tannia130@hotmail.com>
2019-06-26 18:04:59 -0600

    Ropa Alimar

---
+++ Alimar/BL/ManejadorArticulo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL
+{
+    public class ManejadorArticulo
+    {
+        public List<articulo> traerLista() {
+            List<articulo> lista = new List<articulo>();
+
+            //blusas camiseta
+            articulo blusa = new articulo("blusa", "path", "mujer", 10, "Blusa blanca, con lacitos", 5000, "Blusa de niña");
+            articulo blusa2 = new articulo("blusa", "path", "mujer", 30, "", 5000, "Blusa de mujer");
+            articulo blusa3 = new articulo("camisa", "path", "hombre", 10, "", 5000, "Blusa de niña");
+            //short
+
+            //pantalones
+
+            //deportiva
+
+            //vestidos
+
+            //trajebaño
+
+            //pijama
+
+
+
+            return lista;
+        }
+    }
+}
---
+++ Alimar/UI/Busqueda.aspx
@@ -0,0 +1,6 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Principal.Master" AutoEventWireup="true" CodeBehind="Busqueda.aspx.cs" Inherits="UI.Busqueda" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+
+</asp:Content>
