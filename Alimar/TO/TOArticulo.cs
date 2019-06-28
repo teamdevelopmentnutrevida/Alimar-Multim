7480f894ed60d70e41ed81a286d2f6c8f1dc6b0f
juandi perez <juandi199847@hotmail.com>
2019-06-27 22:54:09 -0600

    .

---
+++ Alimar/TO/TOArticulo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TO
+{
+    public class TOArticulo
+    {
+
+        //permite crear un objeto de articulo
+
+        public TOArticulo(string tipo, string path, string genero, double edad, string descipcion, double precio, string nombre)
+        {
+            this.tipo = tipo;
+            this.path = path;
+            this.genero = genero;
+            this.edad = edad;
+            this.descipcion = descipcion;
+            this.precio = precio;
+            this.nombre = nombre;
+        }
+
+        //propiedades de la clase
+        public string tipo { set; get; }
+        public string path { set; get; }
+        public string genero { set; get; }
+        public double edad { set; get; }
+        public string descipcion { set; get; }
+        public double precio { set; get; }
+        public string nombre { set; get; }
+
+        public TOArticulo()
+        {
+        }
+
+    }
+}
